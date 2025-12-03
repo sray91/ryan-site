cbffd33042b99977488216af0f8aecfaf03e0a11
swanagan <swanaganray@gmail.com>
2025-12-03 14:18:31 -0500

    fixed table error

--- sanity.config.js
+++ sanity.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
+import { table } from '@sanity/table';
 import { schemaTypes } from './sanity/schemas';
 
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), table()],
 
   schema: {
     types: schemaTypes,
